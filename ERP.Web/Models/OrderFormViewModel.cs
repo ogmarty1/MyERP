0f02c1321411470e1e76dcd102c9b96645a6cb43
ogmarty1 <martyt369@gmail.com>
2026-07-10 02:00:31 +0300

    View models & helpers: Feat: Implement dynamic Orders management UI with client/server validation and stock sync
    - Add: ERP.Web/Models/OrderFormViewModel.cs

---
+++ ERP.Web/Models/OrderFormViewModel.cs
@@ -0,0 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+using ERP.DataAccess.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ERP.Web.Models
+{
+    public class OrderFormViewModel
+    {
+        [Required(ErrorMessage = "Please select a customer.")]
+        [Display(Name = "Customer")]
+        public int CustomerId { get; set; }
+
+        [Required(ErrorMessage = "Please select who is creating this order.")]
+        [Display(Name = "Created By")]
+        public int UserId { get; set; }
+
+        public List<OrderLineFormViewModel> Lines { get; set; } = new();
+
+        public IEnumerable<SelectListItem> Customers { get; set; } = new List<SelectListItem>();
+        public IEnumerable<SelectListItem> Users { get; set; } = new List<SelectListItem>();
+        public List<Product> ProductCatalog { get; set; } = new();
+    }
+
+    public class OrderLineFormViewModel
+    {
+        [Range(1, int.MaxValue, ErrorMessage = "Please select a valid product.")]
+        public int ProductId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be a positive whole number.")]
+        public int Quantity { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Unit price must be zero or greater.")]
+        public decimal UnitPrice { get; set; }
+    }
+}

fd2226c414771ed7a5f3b220b4f4d2a4270b7fbb
ogmarty1 <martyt369@gmail.com>
2026-07-10 02:45:53 +0300

    Database migrations: Add Categories and Suppliers CRUD nomenclature management
    - Add: ERP.DataAccess/Migrations/20260709233301_AddSupplierContactPerson.Designer.cs
    - Add: ERP.DataAccess/Migrations/20260709233301_AddSupplierContactPerson.cs
    - Update: ERP.DataAccess/Migrations/ApplicationDbContextModelSnapshot.cs

---
+++ ERP.DataAccess/Migrations/20260709233301_AddSupplierContactPerson.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERP.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSupplierContactPerson : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContactPerson",
+                table: "Suppliers",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContactPerson",
+                table: "Suppliers");
+        }
+    }
+}

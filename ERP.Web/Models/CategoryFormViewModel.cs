30a99add0372756410347173a32f9a3127656dcd
ogmarty1 <martyt369@gmail.com>
2026-07-10 02:45:53 +0300

    View models & helpers: Add Categories and Suppliers CRUD nomenclature management
    - Add: ERP.Web/Models/CategoryFormViewModel.cs
    - Add: ERP.Web/Models/SupplierFormViewModel.cs

---
+++ ERP.Web/Models/CategoryFormViewModel.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ERP.Web.Models
+{
+    public class CategoryFormViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [StringLength(500)]
+        public string? Description { get; set; }
+    }
+}
---
+++ ERP.Web/Models/SupplierFormViewModel.cs
@@ -0,0 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ERP.Web.Models
+{
+    public class SupplierFormViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; } = string.Empty;
+
+        [StringLength(150)]
+        [Display(Name = "Company Name")]
+        public string? CompanyName { get; set; }
+
+        [StringLength(150)]
+        [Display(Name = "Contact Person")]
+        public string? ContactPerson { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Tax Number")]
+        public string TaxNumber { get; set; } = string.Empty;
+
+        [StringLength(150)]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [StringLength(30)]
+        [Phone]
+        public string? Phone { get; set; }
+
+        [Display(Name = "Active")]
+        public bool IsActive { get; set; } = true;
+    }
+}

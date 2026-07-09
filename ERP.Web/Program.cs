5d9baf7db84b85428ef55c12c2fb346322003cc2
ogmarty1 <martyt369@gmail.com>
2026-07-10 01:50:50 +0300

    Miscellaneous: Add Products and Customers management UI in ERP.Web
    - Update: ERP.Web/Program.cs

--- ERP.Web/Program.cs
+++ ERP.Web/Program.cs
@@ -12,6 +12,10 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<IInventoryService, InventoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ISupplierService, SupplierService>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
 
 var app = builder.Build();
 

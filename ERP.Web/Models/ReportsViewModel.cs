6720d53276c1f2f2e2f1ce4258bd7fa391dc8c68
ogmarty1 <martyt369@gmail.com>
2026-07-15 18:42:32 +0300

    View models & helpers: Add Dashboard, Sales Reports, and restrict Customers CRUD to Manager/Admin
    - Add: ERP.Web/Models/ReportsViewModel.cs

---
+++ ERP.Web/Models/ReportsViewModel.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using ERP.BusinessLogic.DTOs;
+
+namespace ERP.Web.Models
+{
+    public class ReportsFilterViewModel
+    {
+        [DataType(DataType.Date)]
+        [Display(Name = "Start Date")]
+        public DateTime? StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "End Date")]
+        public DateTime? EndDate { get; set; }
+
+        [Display(Name = "Group By")]
+        public ReportPeriodGrouping Grouping { get; set; } = ReportPeriodGrouping.Monthly;
+    }
+
+    public class ReportsViewModel
+    {
+        public ReportsFilterViewModel Filter { get; set; } = new();
+        public List<SalesByPeriodItem> SalesByPeriod { get; set; } = new();
+        public List<SalesByCategoryItem> SalesByCategory { get; set; } = new();
+    }
+}

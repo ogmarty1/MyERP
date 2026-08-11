e6c806f3e5a195c97e0f4fde746154b51bb1e19b
ogmarty1 <martyt369@gmail.com>
2026-08-12 01:49:20 +0300

    Database migrations: Add User Management module with email-based login and confirm password
    - Add: ERP.DataAccess/Migrations/20260811213145_ReplaceUsernameWithFirstLastNameAndPhone.Designer.cs
    - Add: ERP.DataAccess/Migrations/20260811213145_ReplaceUsernameWithFirstLastNameAndPhone.cs

---
+++ ERP.DataAccess/Migrations/20260811213145_ReplaceUsernameWithFirstLastNameAndPhone.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERP.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReplaceUsernameWithFirstLastNameAndPhone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Username",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Users",
+                newName: "LastName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Users",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Users",
+                newName: "Username");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Username",
+                table: "Users",
+                column: "Username",
+                unique: true);
+        }
+    }
+}
